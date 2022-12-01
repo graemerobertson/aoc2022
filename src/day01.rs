6fd45283eeb9f8168be1b9730e3c41fcc5963fc6
Graeme Robertson <graeme.robertson@metaswitch.com>
2022-12-01 07:27:09 +0000

    Day 01

--- src/day01.rs
+++ src/day01.rs
@@ -4,8 +4,30 @@ use std::io::{BufRead, BufReader};
 pub(crate) fn day01() {
     let f: File = File::open("data/day01.txt").unwrap();
     let reader: BufReader<File> = BufReader::new(f);
-    let mut depths: Vec<u32> = vec![];
+
+    // Assume the data contains at least one elf
+    let mut number_of_elves: usize = 1;
+    let mut calories: Vec<u32> = vec![0; number_of_elves];
+
     for line in reader.lines() {
-        depths.push(line.unwrap().parse::<u32>().unwrap());
+        if let Ok(i) = line.unwrap().parse::<u32>() {
+            calories[number_of_elves - 1] += i;
+        } else {
+            // New elf
+            number_of_elves += 1;
+            calories.push(0);
+        }
     }
+
+    calories.sort();
+    println!(
+        "Elf with the most calories is carrying {} calories",
+        calories.last().unwrap()
+    );
+    println!(
+        "Three elves with the most calories are carrying {} calories",
+        calories[calories.len() - 3..calories.len()]
+            .iter()
+            .sum::<u32>()
+    );
 }
