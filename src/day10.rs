104efbac5d4ce501ae4f0a4c1e2abb1b6f206e52
Graeme Robertson <g@projectclearwater.org>
2022-12-11 16:01:44 +0000

    Day 10

--- src/day10.rs
+++ src/day10.rs
@@ -1,9 +1,50 @@
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{BufRead, BufReader};
 
 pub(crate) fn day10() {
     let f: File = File::open("data/day10.txt").unwrap();
     let reader: BufReader<File> = BufReader::new(f);
-    let _input_data: Vec<String> = reader.lines().collect::<io::Result<Vec<String>>>().unwrap();
-    println!("UNSOLVED");
+
+    let mut operations: Vec<i32> = vec![];
+    for line in reader.lines() {
+        let unwrapped_line = line.unwrap();
+        operations.push(0);
+        if unwrapped_line.starts_with("addx") {
+            operations.push(
+                unwrapped_line
+                    .split_ascii_whitespace()
+                    .last()
+                    .unwrap()
+                    .parse::<i32>()
+                    .unwrap(),
+            )
+        }
+    }
+
+    let mut x: i32 = 1;
+    let mut signal_strength = 0;
+    let mut screen: Vec<char> = vec!['.'; 240];
+    for cycle_number in 1..240 {
+        if (cycle_number + 20) % 40 == 0 {
+            signal_strength += x * cycle_number;
+        }
+        if x - 1 == (cycle_number - 1) % 40
+            || x + 1 == (cycle_number - 1) % 40
+            || x == (cycle_number - 1) % 40
+        {
+            screen[cycle_number as usize - 1] = '#';
+        }
+        x += operations.get(cycle_number as usize - 1).unwrap();
+    }
+
+    println!("Signal strength is {}", signal_strength);
+    println!("Screen:");
+    let mut row = "".to_string();
+    for (index, c) in screen.iter().enumerate() {
+        row.push(*c);
+        if (index + 1) % 40 == 0 {
+            println!("{}", row);
+            row.clear();
+        }
+    }
 }
