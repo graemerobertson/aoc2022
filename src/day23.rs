86f7257e4f5c00086f1c783c48264ba5bc29042a
Graeme Robertson <g@projectclearwater.org>
2022-12-23 13:37:34 +0000

    Simplify day23

--- src/day23.rs
+++ src/day23.rs
@@ -1,5 +1,5 @@
-use std::collections::HashMap;
 use std::collections::VecDeque;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
@@ -15,11 +15,6 @@ enum Direction {
     South,
     West,
     East,
-}
-
-#[derive(Clone, Debug)]
-struct Elf {
-    preferred_direction: VecDeque<Direction>,
 }
 
 const DEFAULT_PREFERRED_DIRECTIONS: [Direction; 4] = [
@@ -68,33 +63,28 @@ pub(crate) fn day23() {
     let f: File = File::open("data/day23.txt").unwrap();
     let reader: BufReader<File> = BufReader::new(f);
     let input_data: Vec<String> = reader.lines().collect::<io::Result<Vec<String>>>().unwrap();
-    let mut elves: HashMap<Coord, Elf> = HashMap::new();
+    let mut elves: HashSet<Coord> = HashSet::new();
     for (ii, line) in input_data.iter().enumerate() {
         for (jj, c) in line.chars().enumerate() {
             if c == '#' {
-                elves.insert(
-                    Coord {
-                        x: jj as i32,
-                        y: ii as i32,
-                    },
-                    Elf {
-                        preferred_direction: VecDeque::from(DEFAULT_PREFERRED_DIRECTIONS.clone()),
-                    },
-                );
+                elves.insert(Coord {
+                    x: jj as i32,
+                    y: ii as i32,
+                });
             }
         }
     }
 
+    let mut preferred_directions = VecDeque::from(DEFAULT_PREFERRED_DIRECTIONS.clone());
     let mut round_count = 0;
     loop {
         round_count += 1;
-        let elves_at_start_of_round = elves.clone();
         let mut proposed_moves_this_round: HashMap<Coord, Vec<Coord>> = HashMap::new();
 
-        for (location, elf) in elves.iter_mut() {
+        for location in &elves {
             let mut elf_should_consider_moving = false;
             for adj_locations in &ADJACENT_LOCATIONS {
-                if elves_at_start_of_round.contains_key(&Coord {
+                if elves.contains(&Coord {
                     x: location.x + adj_locations.x,
                     y: location.y + adj_locations.y,
                 }) {
@@ -103,10 +93,10 @@ pub(crate) fn day23() {
                 }
             }
             if !elf_should_consider_moving {
-                elf.preferred_direction.rotate_left(1);
                 continue;
             }
-            for direction in &elf.preferred_direction {
+
+            for direction in &preferred_directions {
                 let mut elf_should_move_here = true;
                 let current_adj_locations = match direction {
                     Direction::North => ADJACENT_LOCATIONS_NORTH,
@@ -115,7 +105,7 @@ pub(crate) fn day23() {
                     Direction::East => ADJACENT_LOCATIONS_EAST,
                 };
                 for adj_locations in &current_adj_locations {
-                    if elves_at_start_of_round.contains_key(&Coord {
+                    if elves.contains(&Coord {
                         x: location.x + adj_locations.x,
                         y: location.y + adj_locations.y,
                     }) {
@@ -155,24 +145,23 @@ pub(crate) fn day23() {
                     break;
                 }
             }
-            elf.preferred_direction.rotate_left(1);
         }
         let mut move_has_occurred = false;
         for (new_location, old_locations) in proposed_moves_this_round.iter() {
             // Only one elf wants to move to this new location, so let's make it happen
             if old_locations.len() == 1 {
                 move_has_occurred = true;
-                let elf_on_the_move = elves.remove(old_locations.first().unwrap());
-                elves.insert(*new_location, elf_on_the_move.unwrap());
+                elves.remove(old_locations.first().unwrap());
+                elves.insert(*new_location);
             }
         }
         if round_count == 10 {
             println!(
                 "{} empty ground tiles in the elves rectangle after 10 rounds",
-                (elves.keys().map(|c| c.x).max().unwrap() + 1
-                    - elves.keys().map(|c| c.x).min().unwrap())
-                    * (elves.keys().map(|c| c.y).max().unwrap() + 1
-                        - elves.keys().map(|c| c.y).min().unwrap())
+                (elves.iter().map(|c| c.x).max().unwrap() + 1
+                    - elves.iter().map(|c| c.x).min().unwrap())
+                    * (elves.iter().map(|c| c.y).max().unwrap() + 1
+                        - elves.iter().map(|c| c.y).min().unwrap())
                     - elves.len() as i32
             );
         }
@@ -180,5 +169,6 @@ pub(crate) fn day23() {
             println!("The first round where no elves move is {}", round_count);
             return;
         }
+        preferred_directions.rotate_left(1);
     }
 }
